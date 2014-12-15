52dd2cfe2d331a91afcd88915cab9f311552a8b4
laurenskz <laurenskz@gmail.com>
2014-12-15 16:05:02 +0100

    StudyDirection voorzien

--- StudyDirection.java
+++ StudyDirection.java
@@ -1,22 +1,31 @@
 
 /**
- * Enumeration class StudyDirection - write a description of the enum class here
+ * Enumeration class StudyDirection
+ * It contains all Study directions Students can choose from.
  * 
- * @author (your name here)
- * @version (version number or date here)
+ * @author (Laurens op t Zandt)
+ * @version (15/12/2014)
  */
 public enum StudyDirection
 {
+    //All Study directions with a nice String representation so it can be printed later
     ECONOMICS("Economics"), MEDICAL("Medical"), SPORTS("Sports"), PHYSICS("Physics"), 
     UNKNOWN("Unknown");
     
+    //The String holding the nice format of the study direction
     private String directionName;
     
+    /**
+     * Make a new Enumerated type of Study direction with a String direction Name as parameter
+     */
     private StudyDirection(String directionName)
     {
         this.directionName = directionName;
     }
     
+    /**
+     * Returns the directionName of an enumerated type StudyDirection
+     */
     public String getDirectionName()
     {
         return directionName;
