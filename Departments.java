608899c817b92aa7f92d59d2136d9628b3e7e21e
laurenskz <laurenskz@gmail.com>
2014-12-15 16:47:22 +0100

    klaar

--- Departments.java
+++ Departments.java
@@ -1,20 +1,28 @@
 
 /**
- * Enumeration class DepartMents - write a description of the enum class here
+ * Enumeration class DepartMents 
  * 
- * @author (your name here)
- * @version (version number or date here)
+ * @author (Laurens op t Zandt & Joost van Woensel)
+ * @version (15/12/2014)
  */
 public enum Departments
 {
+    //All Departments with a nice String representation so it can be printed later
     SPORTS("Sports"), COUNSELING("Counseling"), ENGLISH("English"), MATH("Math"), UNKNOWN("Unknown");
+    //The String holding the nice format of the department
     private String departmentName;
     
+    /**
+     * Make a new Enumerated type of Departments with a String direction Name as parameter
+     */
     private Departments(String departmentName)
     {
         this.departmentName = departmentName;
     }
     
+    /**
+     * Returns the departmentName of an enumerated type Departments
+     */
     public String getDepartmentName()
     {
         return departmentName;
