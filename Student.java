58ff758a06ecc5115c48ba551aa1ffd9d98f7019
laurenskz <laurenskz@gmail.com>
2014-12-15 16:11:06 +0100

    Student klaar

--- Student.java
+++ Student.java
@@ -1,18 +1,20 @@
 
 /**
- * Write a description of class Student here.
+ * A class containing data about students.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Laurens op t Zandt & Joost van Woensel)) 
+ * @version (15/12/2014)
  */
 public class Student extends Person
 {
     //A variable holding the enumerated type of study direction
     private StudyDirection studyDirection;
+    //A String holding the student number
     private String studentNumber;
 
     /**
      * Constructor for objects of class Student
+     * It creates an empty student for testing purposes
      */
     public Student()
     {
@@ -21,6 +23,10 @@ public class Student extends Person
         studentNumber = "Unknown";
     }
     
+    /**
+     * Constructor for objects of class Student
+     * It creates a Student object (Which extends a person object) holding all variables given.
+     */
     public Student(int birthDay, int birthMonth, int birthYear, String firstName ,String lastName, char gender, int bsn, StudyDirection studyDirection,
     String studentNumber)
     {
@@ -29,26 +35,41 @@ public class Student extends Person
         this.studentNumber = studentNumber;
     }
     
+    /**
+     * Return the study direction a a student.
+     */
     public StudyDirection getStudyDirection()
     {
         return studyDirection;
     }
     
+    /**
+     * Sets the studyDirection of a student, takes Enumerated type as input.
+     */
     public void setStudyDirection(StudyDirection studyDirection)
     {
         this.studyDirection = studyDirection;
     }
     
+    /**
+     * Returns a students student number
+     */
     public String getStudentNumber()
     {
         return studentNumber;
     }
     
+    /**
+     * Replaces the studentNumber by the given input String.
+     */
     public void setStudentNumber(String studentNumber)
     {
         this.studentNumber = studentNumber;
     }
     
+    /**
+     * Prints all details about the student, overrides to printDetails method from person.
+     */
     public void printDetails()
     {
         System.out.println("#######################################");
